0438a8dd5c8ec2e6e6abb8e17951c056f113bf9d
Hans <hanschristiansudjana1@hotmail.sg>
2020-12-09 23:17:03 +0800

    added solver

--- day1.py
+++ day1.py
@@ -1,3 +1,16 @@
-print("afasf")
+from itertools import combinations
+import math
 
-print(5+3)
+
+
+
+f = open("E:\\Downloads\\ddd.txt")
+x = map(int,f.read().split())
+
+
+def solver(list1, n):
+    for c in combinations(list1, n):
+        if(sum(c) == 2020):
+            return (math.prod(c))
+
+print(solver(x,2))
